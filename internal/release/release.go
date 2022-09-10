410dde405a2fe1bf56ae5519821ac165ba522528
Fabian Mettler <fabian@mettler.cc>
2022-09-10 15:25:46 +0200

    FIX: Typo

--- internal/release/release.go
+++ internal/release/release.go
@@ -1,6 +1,6 @@
 package release
 
-// Release reprsents a release
+// Release represents a release
 type Release struct {
 	LastKnownVersion string `toml:"last_known_version"`
 	GitHubRepo       string `toml:"github_repo"`
